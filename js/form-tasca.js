85a167440232e514b83c4bb23e7d057c1c35d2b7
Nahuel <gerreropiconi@gmail.com>
2026-04-30 11:06:40 +0200

    feat: creació de tasques fetes, s'emagatzemen al local storage

--- js/form-tasca.js
+++ js/form-tasca.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const submitTask = document.getElementById("createTask");
 
     submitTask.addEventListener("submit", function(event) {
-        event.preventDefault;
+        event.preventDefault();
 
         const title = document.getElementById("title");
         const description = document.getElementById("description");
@@ -13,6 +13,27 @@ document.addEventListener("DOMContentLoaded", function() {
         const category = document.getElementById("category");
         const priority = document.getElementById("priority");
 
-        
+        const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+
+        if (checkRepited(title, tasks)) {
+            tasks.push(new Tasca(title.value, description.value ||null, date.value || null, category.value || null, priority.value || null));
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            
+            title.value = "";
+            description.value = "";
+            date.value = "";
+            category.value = "";
+        } else {
+            alert("Aquest nom de tasca ja existeix!");
+        }
     })
+
+    function checkRepited(title, tasks) {
+        for (let i = 0; i < tasks.length; i++) {
+            if (tasks[i]._title == title.value) {
+                return false;
+            }
+        }
+        return true;
+    }
 })
