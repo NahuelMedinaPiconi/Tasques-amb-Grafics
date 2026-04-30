004ebce70dfd9e777dc01ed936a0f4603cfd16c2
Nahuel <gerreropiconi@gmail.com>
2026-04-30 19:03:02 +0200

    feat: mes funcions d'afegir tasques i categories i mapped tasks en storage

--- js/storage.js
+++ js/storage.js
@@ -13,10 +13,27 @@ export function getMappedCategories() {
         .map(category => new Category(category._name, category._color));
 }
 
+export function getMappedTasks() {
+    return JSON.parse(localStorage.getItem("categories"))
+        .map(task => new Tasca(task._title, task._category, task._date, task._description, task._priority, task._finished));
+}
+
 export function setTasks(tasks) {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 export function setCategories(categories) {
     localStorage.setItem("categories", JSON.stringify(categories));
+}
+
+export function addTask(task) {
+    const tasks = getTasks();
+    tasks.push(task);
+    setTasks(task);
+}
+
+export function addCategory(category) {
+    const categories = getCategories();
+    categories.push(category);
+    setTasks(categories);
 }
