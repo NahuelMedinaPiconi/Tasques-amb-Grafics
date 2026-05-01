7fa1f0b2a0ee1a5d94b78007b0a0a58548159516
Nahuel <gerreropiconi@gmail.com>
2026-05-01 13:00:19 +0200

    feat: ara s'amagatzema la variable que defineix si una tasca està finalitzada o no

--- js/models.js
+++ js/models.js
@@ -2,13 +2,13 @@ import { getCategories, setCategories, getTasks, setTasks } from "./storage.js";
 
 export class Tasca {
 
-    constructor(title, category, date, description, priority) {
+    constructor(title, category, date, description, priority, finished = false) {
         this._title = title;
         this._category = category;
         this._date = date;
         this._description = description;
         this._priority = priority;
-        this._finished = false;
+        this._finished = finished;
     }
 
     get title() {
@@ -92,6 +92,12 @@ export class Tasca {
             }
         }
 
+        //Posarli un estil en cas de estar finalitzada
+        if (this._finished) {
+            base.classList.add("finished");
+            pTitle.style.textDecoration = "line-through";
+        }
+
         pTitle.textContent = this._title;
         sPriority.textContent = this._priority;
         sCategory.textContent = this._category;
@@ -100,16 +106,6 @@ export class Tasca {
         divIcons.innerHTML += '' +
         '<svg id="complete" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 14L9 19L20 8M6 8.88889L9.07692 12L16 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>' +
         '<svg id="delete" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M14 10V17M10 10V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>';
-
-        divIcons.querySelector("#complete").addEventListener("click", function() {
-            this._finished = !this._finished;
-            base.classList.toggle("finished");
-            if (this._finished) {
-                pTitle.style.textDecoration = "line-through";
-            } else {
-                pTitle.style.textDecoration = "none";
-            }
-        })
 
         const title = this._title;
         divIcons.querySelector("#delete").addEventListener("click", function() {
@@ -120,6 +116,26 @@ export class Tasca {
                     tasks.splice(i, 1);
                     setTasks(tasks);
                     base.remove();
+                    return;
+                }
+            }
+        })
+
+        //Canviam l'estat de la tasca en cas de que es pulsi el boto.
+        divIcons.querySelector("#complete").addEventListener("click", function() {
+            this._finished = !this._finished;
+            base.classList.toggle("finished");
+
+            pTitle.style.textDecoration = base.classList.contains("finished") ? "line-through" : "none";
+
+            console.log(pTitle.style.textDecoration);
+
+            const tasks = getTasks();
+
+            for (let i = 0; i < tasks.length; i++) {
+                if (tasks[i]._title == title) {
+                    tasks[i]._finished = this._finished;
+                    setTasks(tasks);
                     return;
                 }
             }
