22e2742e176c8ab09aacc64cba07dc4c9b2a67eb
Nahuel <gerreropiconi@gmail.com>
2026-05-02 19:07:55 +0200

    fix: les tasques es posicionen correctament segons si estan acabades o no en temps real

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 import { getMappedTasks } from "./storage.js";
 
+let tasks = getMappedTasks();
+
 document.addEventListener("DOMContentLoaded", function() {
 
     printTasks();
+    
+    document.addEventListener("click", function(event) {
+        if (event.target.closest(".complete")) {
+            tasks = getMappedTasks();
+            printTasks();
+        }
+    });
 
     function printTasks() {
-
-        const tasks = getMappedTasks();
 
         const base = document.getElementById("taskList");
         const baseUnfinished = document.getElementById("unfinishedTasks");
 
         base.innerHTML = "";
+        baseUnfinished.innerHTML = "";
 
         tasks.forEach((task, index) => {
             if (!task._finished) {
--- js/models.js
+++ js/models.js
@@ -108,11 +108,11 @@ export class Tasca {
         pDate.textContent = this.date;
         pDescription.textContent = this._description;
         divIcons.innerHTML += '' +
-        '<svg id="complete" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 14L9 19L20 8M6 8.88889L9.07692 12L16 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>' +
-        '<svg id="delete" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M14 10V17M10 10V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>';
+        '<svg class="complete" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 14L9 19L20 8M6 8.88889L9.07692 12L16 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>' +
+        '<svg class="delete" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M14 10V17M10 10V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>';
 
         const title = this._title;
-        divIcons.querySelector("#delete").addEventListener("click", function() {
+        divIcons.querySelector(".delete").addEventListener("click", function() {
             const tasks = getTasks();
 
             for(let i = 0; i < tasks.length; i++) {
@@ -126,10 +126,8 @@ export class Tasca {
         })
 
         //Canviam l'estat de la tasca en cas de que es pulsi el boto.
-        divIcons.querySelector("#complete").addEventListener("click", () => {
-            console.log(this._finished);
+        divIcons.querySelector(".complete").addEventListener("click", () => {
             this._finished = !this._finished;
-            console.log(this._finished);
             base.classList.toggle("finished");
 
             pTitle.style.textDecoration = base.classList.contains("finished") ? "line-through" : "none";
@@ -143,6 +141,7 @@ export class Tasca {
                     return;
                 }
             }
+
         })
 
         divTop.appendChild(pTitle);
